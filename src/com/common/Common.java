b283e1c7ac88a32fe70debb8146e1619bf1bf985
sunsingh <sunsingh@adobe.com>
2017-02-13 09:51:42 +0530

    Common file is modified by dev 2

--- src/com/common/Common.java
+++ src/com/common/Common.java
@@ -1,9 +1,17 @@
 package com.common;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  *
  * @author Sunil
  */
 public class Common {
-
+        List<String> list = new ArrayList<>();
+        list.add("One");
+        list.add("Two");
+        for(String item : list){
+            System.out.println("ITEM : " + item);
+        }
 }
