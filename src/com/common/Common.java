dab2690997d17610f739ee0bded452394bfc89b7
sunsingh <sunsingh@adobe.com>
2017-02-13 09:47:38 +0530

    Common file is changed

--- src/com/common/Common.java
+++ src/com/common/Common.java
@@ -5,5 +5,10 @@ package com.common;
  * @author Sunil
  */
 public class Common {
+    public static void main(String[] args) {
+        for (int count = 0; count < 5; count++) {
+            System.out.println("COUNT : " + count);
+        }
 
+    }
 }
